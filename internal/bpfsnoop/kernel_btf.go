1b7378eea4137eba9275985094b4c3d5ad43c812
runsisi <runsisi@gmail.com>
2025-08-22 23:05:59 +0800

    enable to load non-split btf files for kernel modules
    Signed-off-by: runsisi <runsisi@gmail.com>

--- internal/bpfsnoop/kernel_btf.go
+++ internal/bpfsnoop/kernel_btf.go
@@ -4,10 +4,12 @@
 package bpfsnoop
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
 	"slices"
+	"strings"
 
 	"github.com/cilium/ebpf/btf"
 )
@@ -73,5 +75,82 @@ func iterateKernelBtfs(allKmods bool, kmods []string, iter func(*btf.Spec) bool)
 		iter(kernelBtf)
 	}
 
+	kmodBtfs, err := LoadKmodBtfs(kfuncKmods, kmodBTFDir)
+	if err != nil {
+		return err
+	}
+	for _, kmodBtf := range kmodBtfs {
+		if iter(kmodBtf) {
+			break // stop iterating if the iterator returns true
+		}
+	}
+
 	return nil
+}
+
+func LoadKmodBtfs(kmods []string, kmodBTFDir string) (map[string]*btf.Spec, error) {
+	if kmodBTFDir == "" {
+		return nil, nil
+	}
+
+	btfs := map[string]*btf.Spec{}
+	if len(kmods) != 0 {
+		kmods = sortCompact(kmods)
+		if idx := slices.Index(kmods, "vmlinux"); idx != -1 {
+			kmods = slices.Delete(kmods, idx, idx+1)
+		}
+
+		btfs = make(map[string]*btf.Spec, len(kmods))
+		for _, kmod := range kmods {
+			btfPath := filepath.Join(kmodBTFDir, kmod+".btf")
+			if !fileExists(btfPath) {
+				return nil, fmt.Errorf("split BTF file %s does not exist", btfPath)
+			}
+
+			kmodBtf, err := btf.LoadSpec(btfPath)
+			if err != nil {
+				if errors.Is(err, btf.ErrNotFound) {
+					WarnLog("BTF not found for %s", btfPath)
+					continue
+				}
+				return nil, fmt.Errorf("failed to load %s btf: %w", btfPath, err)
+			}
+
+			btfs[kmod] = kmodBtf
+		}
+	} else {
+		files, err := os.ReadDir(kmodBTFDir)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read /sys/kernel/btf: %w", err)
+		}
+
+		fileNames := make([]string, 0, len(files))
+		for _, file := range files {
+			if file.IsDir() || file.Name() == "vmlinux" {
+				continue // skip directories and vmlinux
+			}
+			if !strings.HasSuffix(file.Name(), ".btf") {
+				continue
+			}
+			fileNames = append(fileNames, file.Name())
+		}
+
+		btfs = make(map[string]*btf.Spec, len(fileNames))
+		for _, fileName := range fileNames {
+			btfPath := filepath.Join(kmodBTFDir, fileName)
+
+			kmodBtf, err := btf.LoadSpec(btfPath)
+			if err != nil {
+				if errors.Is(err, btf.ErrNotFound) {
+					WarnLog("BTF not found for %s", btfPath)
+					continue
+				}
+				return nil, fmt.Errorf("failed to load %s BTF: %w", btfPath, err)
+			}
+
+			btfs[strings.TrimSuffix(fileName, ".btf")] = kmodBtf
+		}
+	}
+
+	return btfs, nil
 }
